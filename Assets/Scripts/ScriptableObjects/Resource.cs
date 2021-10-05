cedd83f1349a88c420997b67eaf682baa8d28146
ArlingtonDawson <arlington.dawson@gmail.com>
2021-10-05 16:15:40 -0400

    Added Resource and Research Item Scriptiable Objects. (#11)

--- Assets/Scripts/ScriptableObjects/Resource.cs
+++ Assets/Scripts/ScriptableObjects/Resource.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Resources", menuName = "LetsDoThis/Resource")]
 public class Resource : ScriptableObject
 {
-    public string Name;
+    public string DisplayName;
 }
